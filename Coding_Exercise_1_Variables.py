ec82bdbb3274b09cdca1d510cd89f5b85114a8cf
tansyDeveloper06-Salman-Ali <48586129+Salman-AIi@users.noreply.github.com>
2019-08-21 09:15:34 +0530

    Delete Coding_Exercise_1_Variables.py

--- Coding_Exercise_1_Variables.py
+++
@@ -1,8 +0,0 @@
-# Create two variables, var1 and var2, both with the same value.
-var1 = 4
-var2 = 4
-
-# Create two variables, num1 and num2, which multiply together to give 16.
-num1 = 4
-num2 = 4
-print(num1 * num2)
