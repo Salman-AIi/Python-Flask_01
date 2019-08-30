08083e0e3f8eaebb7a2ea5ca2c759161b6ebba36
Decopied <salmanalimd123@gmail.com>
2019-08-30 15:36:37 +0530

    homie

---
+++ Rough/code/security.py
@@ -0,0 +1,47 @@
+from werkzeug.security import safe_str_cmp
+from user import User
+
+
+
+
+
+def authenticate(username,password):
+    user = User.find_by_username(username)
+    if user and safe_str_cmp(user.password,password):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return User.find_by_id(user_id)
+
+
+
+
+#
+# users = {
+#     Users(1 , 'nifty' , 'noob457')
+#
+# }
+#
+#
+# user_mapping = {u.username: u for u in users}
+# userid_mapping = {u.id: u for u in users}
+# # users ={
+# #
+# #     'id'=1,
+# #     'username'='nifty',
+# #     'password'='noob457'
+# # }
+# #
+# # user_mapping = {'nifty':{
+# #         'id'=1,
+# #         'username'='nifty',
+# #         'password'='noob457'
+# #     }
+# # }
+# #
+# # userid_mapping = {1:{
+# #     'id'=1,
+# #     'username'='nifty',
+# #     'password'='noob457'
+# # }}
--- Section 5/code/app.py
+++ Section 5/code/app.py
@@ -1,51 +1,16 @@
-from flask import Flask ,request
-from flask_restful import Resource,Api,reqparse
-from flask_jwt import JWT , jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from security import authenticate , identity
-items =[]
+from user import UserRegister
+from items import Item,ItemList
 app = Flask(__name__)
 app.secret_key = 'noob'
 api = Api(app)
 jwt = JWT(app,authenticate,identity)
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help='This Field Is Required!'
-    )
-    @jwt_required()
-    def get(self,name):
-        item = next(filter(lambda x: x['name'] == name,items),None)
-        return {"item":item},200 if item else 404
-    def post(self,name):
-        print(items)
-        if next(filter(lambda x: x['name']==name,items),None):
-            return {'message':"An Item With Name'{}' Already Exists.".format(name)}, 400
-        data = Item.parser.parse_args()
-        # item = {'name':name,'price':data['price']}
-        item = {'name':name,'price':12}
-        items.append(item)
-        return item,201
-    def delete(self,name):
-        global items
-        items = list(filter(lambda x: x['name'] != name,items))
-        return {'message':'The Item Has Been Deleted Fam!'}
-    def put(self,name):
-
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x:x['name'] == name,items), None)
-        if item is None:
-            item = {'name':name,'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-class ItemList(Resource):
-    def get(self):
-        return {'items':items}
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/item')
-app.run(port=5000,debug=True)
+api.add_resource(UserRegister,'/register')
+
+if __name__ == '__main__':    
+    app.run(port=5000,debug=True)
